b3a607f3f67289c0b597c75e2ad031b7f5cc3589
Andrias Siwabessy <andriasmelianus@gmail.com>
2019-12-05 10:22:37 +0700

    Modifikasi handler untuk menyesuaikan mekanisme error reporting pada API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,15 +2,20 @@
 
 namespace App\Exceptions;
 
+use App\Alice\ApiResponser;
 use Exception;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
+    private $apiResponser;
     /**
      * A list of the exception types that should not be reported.
      *
@@ -22,6 +27,10 @@ class Handler extends ExceptionHandler
         ModelNotFoundException::class,
         ValidationException::class,
     ];
+
+    public function __construct(ApiResponser $apiResponser) {
+        $this->apiResponser = $apiResponser;
+    }
 
     /**
      * Report or log an exception.
@@ -43,8 +52,43 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
      */
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
+    public function render($request, Exception $exception) {
+        if($exception instanceof HttpException){
+            $code = $exception->getStatusCode();
+            $message = Response::$statusTexts[$code];
+
+            return $this->apiResponser->error($message, $code);
+        }
+
+        if($exception instanceof ModelNotFoundException){
+            $model = strtolower(class_basename($exception->getModel()));
+
+            return $this->apiResponser->error('Model '.$model.' not found', Response::HTTP_NOT_FOUND);
+        }
+
+        if($exception instanceof AuthorizationException){
+            return $this->apiResponser->error($exception->getMessage(), Response::HTTP_FORBIDDEN);
+        }
+        if($exception instanceof AuthenticationException){
+            return $this->apiResponser->error($exception->getMessage(), Response::HTTP_UNAUTHORIZED);
+        }
+
+        if($exception instanceof ValidationException){
+            $errors = $exception->validator->errors()->getMessages();
+            return $this->apiResponser->error($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        if($exception instanceof ClientException){
+            $message = $exception->getResponse()->getBody();
+            $code = $exception->getCode();
+
+            return $this->apiResponser->errorMessage($message, $code);
+        }
+
+        if(env('APP_DEBUG', FALSE)){
+            return parent::render($request, $exception);
+        }
+
+        return $this->apiResponser->error('Unexpected error. Try later!', Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
