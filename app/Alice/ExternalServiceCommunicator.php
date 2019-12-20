aef7c8fe53180e6ad8de997cfd44329590cdaf91
Andrias Siwabessy <andriasmelianus@gmail.com>
2019-12-20 23:00:43 +0700

    Penambahan modul yang memungkinkan komunikasi antar microservices.

---
+++ app/Alice/ExternalServiceCommunicator.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Alice;
+
+use GuzzleHttp\Client;
+
+class ExternalServiceCommunicator {
+
+    private $baseUri;
+    private $secret;
+
+    /**
+     * Constructor
+     *
+     * @param String $baseUri
+     * @param String $secret
+     */
+    public function __construct($baseUri, $secret){
+        $this->baseUri = $baseUri;
+        $this->secret = $secret;
+    }
+
+    /**
+     * Menjalankan request dari Gateway ke service lain
+     *
+     * @param String $method
+     * @param String $requestUrl
+     * @param array $params
+     * @return void
+     */
+    public function performRequest($method, $requestUrl, $params){
+        $client = new Client([
+            'base_uri' => $this->baseUri
+        ]);
+
+        /**
+         * Pemasangan secret
+         */
+        if(isset($this->secret)){
+            $params['headers'] = [
+                'Authorization' => "Bearer {$this->secret}"
+            ];
+        }
+
+        $response = $client->request($method, $requestUrl, $params);
+
+        return $response->getBody()->getContents();
+    }
+}
